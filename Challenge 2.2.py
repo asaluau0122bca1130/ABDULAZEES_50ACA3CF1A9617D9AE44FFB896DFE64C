31670d679574e9ddeefc87feab3f800a523b3c0f
asaluau0122bca1130 <144206992+asaluau0122bca1130@users.noreply.github.com>
2023-09-19 19:01:05 +0530

    Add files via upload

---
+++ Challenge 2.2.py
@@ -0,0 +1,28 @@
+# Define the basic class player
+class player:
+
+  def play(self):
+    print("The player is playing cricket.")
+
+
+# Define the derived class Batsman
+class Batsman(player):
+
+  def play(self):
+    print("The batsman is batting.")
+
+
+# Define the derived class Bowler
+class Bowler(player):
+
+  def play(self):
+    print("The bowler is bowling.")
+
+
+#Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#Call the player() method for each object
+batsman.play()
+bowler.play()
